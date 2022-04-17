7ec2739257e24ed701b11a04542cc9ff67e50491
Corey Gettig <coreygettig7@gmail.com>
2022-04-17 19:27:52 -0400

    added fs module and added ability tfor server to store notes, tested on the localhost

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,37 @@
 const router = require('express').Router();
+const fs = require('fs');
 
-
-const { notes } = require('../db/db.json');
+// const notes = require('../db/db.json');
 
 // GET route to grab notes from the notes database
 router.get('/notes', (req, res) => {
-    let results = notes;
+    let notes = fs.readFileSync('./db/db.json', 'utf8');
 
-    res.json(results);
+    res.json(JSON.parse(notes));
 });
 
 router.post('/notes', (req, res) => {
-    console.log(req.body);
+    let notes = fs.readFileSync('./db/db.json', 'utf8');
 
-    res.json(req.body);
-})
+    const note = {
+        ...req.body,
+        id: notes.length.toString()
+    };
+
+    const parsedNotes = JSON.parse(notes);
+
+    parsedNotes.push(note);
+
+    fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 2),
+        (err, text) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log("You added a note!");
+        });
+
+    res.json(note);
+});
 
 module.exports = router;
